9d7fdabee53838959c071965a0d304d2a469e0a5
Hash543 <alishaqoo@gmail.com>
2016-05-03 03:05:10 +0800

    修改刷條碼使用行為

--- test.js
+++ test.js
@@ -1,29 +1,24 @@
-var exec = require('child_process').exec,
-    child;
-var http = require('http');
-exec("python test.py" , function(err ,data ,c){
-    error = err;
-    if(err == null && data.length > 0){
-    	data = data.trim();
-    	var options = {
-		  hostname: '61.220.182.219',
-		  port: 80,
-		  path: '/api/custByEmp/000/000/'+data
-		};
-    	var req = http.request(options, function(res) {
-		  	console.log('STATUS: ' + res.statusCode);
-		  	console.log('HEADERS: ' + JSON.stringify(res.headers));
-		  	res.setEncoding('utf8');
-		  	res.on('data', function (chunk) {
-		  		console.log(chunk);
-		    	sc = JSON.parse(chunk)[0];
-		    });
-		});
-		req.on('error', function(e) {
-		  console.log('problem with request: ' + e.message);
-		});
-
-		req.end();
+var GitHubApi = require("github");
+ 
+var github = new GitHubApi({
+    // required 
+    version: "3.0.0",
+    // optional https://github.com/Hash543/yscounter.git
+    debug: true,
+    protocol: "https",
+    host: "github.com", // should be api.github.com for GitHub 
+    pathPrefix: "/api/v3", // for some GHEs; none for GitHub 
+    timeout: 5000,
+    headers: {
+        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent 
     }
-    console.log(data);
+});
+github.user.getFollowingFromUser({
+    // optional: 
+    // headers: { 
+    //     "cookie": "blahblah" 
+    // }, 
+    user: "mikedeboer"
+}, function(err, res) {
+    console.log(JSON.stringify(res));
 });
