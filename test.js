6e1f99018491b5eeb5b6584ed83223d24a34e2f7
Hash543 <alishaqoo@gmail.com>
2016-06-22 10:09:33 +0800

    code change

--- test.js
+++ test.js
@@ -1,23 +1,3 @@
-var download = require("url-download");
-var unzip = require("unzip");
-var fs = require("fs");
-var i = 0;
-download('https://github.com/Hash543/yscounter/archive/master.zip', './')
-    .on('close', function () {
-      console.log('One file has been downloaded.');
-      //fs.createReadStream('master.zip').pipe(unzip.Extract({ path: '../test/download' }));
-      fs.createReadStream('master.zip')
-        .pipe(unzip.Parse())
-        .on('entry', function (entry) {
-          var fileName = entry.path;
-          var type = entry.type; // 'Directory' or 'File' 
-          var size = entry.size;
-          //console.log(fileName.replace("yscounter-master",""));
-          if (fileName.match(/\.js$/)) {
-            i++;
-            entry.pipe(fs.createWriteStream('.'+fileName.replace("yscounter-master","")));
-          } else {
-            entry.autodrain();
-          }
-        });
-    });
+var HID = require('node-hid');
+var devices = HID.devices();
+console.log(devices);
