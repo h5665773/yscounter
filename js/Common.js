c7254a95c503998673cff7fdf70e47b77c6bd5ec
Hash543 <alishaqoo@gmail.com>
2016-06-24 18:05:48 +0800

    現場班表以及公司名稱

--- js/Common.js
+++ js/Common.js
@@ -1,9 +1,23 @@
+var http = require('http');
+var config = require("./config.js");
 var fs = require('fs');
 var jade = require("jade");
 var custFile = "public/custid.txt" ;
 var currentCust = fs.readFileSync(custFile , {encoding:'utf-8'});
 $(document).ready(function(){
-  var navJade = jade.compileFile("views/header.jade");
-  var navHtml = navJade({current: "checkin"});
-  $("nav").html(navHtml);
+	http.get(config.apiHost + 'api/custName/'+currentCust+'/0/0', (res) => {
+      var resContent="";
+      res.on('data',(d) =>{
+        resContent+=d;
+        //$("#message").html(news[0].F_MESSAGE);
+      });
+      res.on('end',function(){
+        var cust = JSON.parse(resContent);
+        $("#companyName").html(cust[0].F_CU_SNAME)
+      });
+      res.resume();
+    });
+	var navJade = jade.compileFile("views/header.jade");
+	var navHtml = navJade({current: "checkin"});
+	$("nav").html(navHtml);
 });
