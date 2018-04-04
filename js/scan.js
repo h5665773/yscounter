7f4f67030575f6c8d07e609bcf0d97720e453ecc
Hash543 <alishaqoo@gmail.com>
2018-04-05 03:13:05 +0800

    瀏覽班表需要先刷卡

---
+++ js/scan.js
@@ -0,0 +1,26 @@
+!function () {
+  	'use strict';
+	var exec = require('child_process').exec,
+          child;
+	module.exports = function (cb) {
+		var scanEmpId ="";
+		exec("python scan.py" , function(err ,data ,c){
+	        if(err == null && data.length > 0){
+
+	          data = data.trim();
+	          var parse = data.match(/([A-Z]{1}[0-9]+)([A-Z]?)/);
+	          //cb(parse,false);
+	          if(parse == null){
+	          	//error callback
+	          	cb("刷入資料錯誤",true);
+	          }else{
+	          	scanEmpId = parse[1];
+	          	cb(scanEmpId,false);
+	          }
+	        }else{
+	          //error callback
+	          cb(err,true);
+	        }
+	    });
+	};
+}();
