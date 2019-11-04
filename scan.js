d1b93bc5cff0efef2867ee020354ff78214d8f92
Hash543 <alishaqoo@gmail.com>
2019-11-04 18:13:54 +0800

    20191104update

---
+++ scan.js
@@ -0,0 +1,50 @@
+const SerialPort = require('serialport')
+const fs = require("fs");
+var com = fs.readFileSync('./public/comport.txt').toString()
+var d = new Date()
+var end = new Date()
+const startSec = d.getTime()
+var returnStr ="";
+const port = new SerialPort('com'+com);
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+var checkClose = function(){
+	//console.log(e.getTime() + "-" +startSec + " = " + (e.getTime()-startSec));
+	e = new Date()
+	if(returnStr !=""){
+		console.log(666);
+		console.log(returnStr);
+		startSec = 0;
+	}else{
+		
+		if(e.getTime()-startSec > 20000){
+			console.log(444);
+			console.log(port.closing);
+			if(port.closing == false){
+				console.log(port);
+				port.close();
+			}  
+		}
+	}
+}
+port.timeout = 20000;
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  returnStr=data.toString().substr(0,6)
+  console.log(returnStr);
+})
+
+port.on('error',function(e){
+	console.log(e);
+});
+port.on('open',function(){
+	var refreshIntervalId = setInterval(function(){
+		end = new Date()
+		if(end.getTime()-startSec > 20000 || returnStr !=""){
+			port.close();
+			clearInterval(refreshIntervalId);
+		}
+	}, 1000);
+
+	/* later */
+	
+});
