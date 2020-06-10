78c3a1e9b0567e83118736d6bfb0db0af718fa64
Hash543 <alishaqoo@gmail.com>
2020-06-10 12:53:42 +0800

    Update handover.js

--- js/handover.js
+++ js/handover.js
@@ -243,8 +243,8 @@ $(document).ready(function(){
       	loading.css("display","block");
       	loading.addClass("rotateIn");
       	scan((empId,err) => {
-      		err = false;
-      		empId = "T10220";
+      		//err = false;
+      		//empId = "T10220";
       		loadingMask.css("display","none");
             loading.css("display","none");
             loading.removeClass("rotateIn");
