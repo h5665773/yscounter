6510f790c805578d44f540a3329acddba26104b3
WeiJay <jason10391039@gmail.com>
2020-10-22 09:51:59 +0800

    HiddenEmployeeWarning

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
 	apiHost: "http://api.wyattst.com.tw/",
 	ajaxOrder: "http://admin.wyattst.com.tw/YSWEB/XMLFORM/AjaxOrder.aspx",
 	updateURL: "https://github.com/h5665773/yscounter/archive/master.zip",
+	testURL: "http://localhost:50381/XMLFORM/AjaxOrder.aspx"
 };
