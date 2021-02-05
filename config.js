fd2137ce6c12cd74e04d15db63cf0738bfdf3bf1
WeiJay <jason10391039@gmail.com>
2021-02-05 15:40:20 +0800

    cust修正

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apiHost: "http://api.wyattst.com.tw/",
-	ajaxOrder: "http://admin.wyattst.com.tw/YSWEB/XMLFORM/AjaxOrder.aspx",
+	ajaxOrder: "http://admin.wyattst.com.tw//YSWEB/JSONFORM/YsCounterApi.aspx",
 	updateURL: "https://github.com/h5665773/yscounter/archive/master.zip",
 	CMSOrder: "http://admin.wyattst.com.tw/CMS/Api/AjaxOrder",
-	testURL: "http://localhost:50381/XMLFORM/AjaxOrder.aspx"
+	testURL: "http://localhost:50381/JSONFORM/YsCounterApi.aspx"
 };
