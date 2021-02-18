32c4a355e2bdf118ad874daf4181600e1274272e
Wes <38410738+h5665773@users.noreply.github.com>
2021-02-18 14:56:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apiHost: "http://api.wyattst.com.tw/",
-	ajaxOrder: "http://admin.wyattst.com.tw//YSWEB/JSONFORM/YsCounterApi.aspx",
+	ajaxOrder: "http://admin.wyattst.com.tw//YSWEB/XMLFORM/YsCounterApi.aspx",
 	updateURL: "https://github.com/h5665773/yscounter/archive/master.zip",
 	CMSOrder: "http://admin.wyattst.com.tw/CMS/Api/AjaxOrder",
-	testURL: "http://localhost:50066/JSONFORM/YsCounterApi.aspx"
+	testURL: "http://localhost:50066/XMLFORM/YsCounterApi.aspx"
 };
