009b5ee3efe02207f5ae6fb791946c0ec89e6e32
Hash543 <alishaqoo@gmail.com>
2021-11-29 13:46:14 +0800

    教育訓練登錄

--- test.js
+++ test.js
@@ -1,2 +1,22 @@
-const screenshot = require('screenshot-desktop')
-screenshot({ filename: './screen.png' })
+const request = require('request')
+function doRequest(url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body.substr(0,10));
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// Usage:
+
+async function main() {
+  let res = await doRequest('https://www.google.com');
+  console.log(res);
+  console.log(123)
+}
+
+main();
