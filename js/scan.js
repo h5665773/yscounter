eb02cd04b27e1000f9a3ce0d62597d5878491b06
Wes <38410738+h5665773@users.noreply.github.com>
2023-09-15 14:10:04 +0800

    modify:掃條碼兼容台南員工ID

--- js/scan.js
+++ js/scan.js
@@ -8,7 +8,7 @@
 	        if(err == null && data.length > 0){
 
 	          data = data.trim();
-	          var parse = data.match(/([A-Z]{1}[0-9]+)([A-Z]?)/);
+	          var parse = data.match(/([A-Z]{1,2}[0-9]+)([A-Z]?)/);
 	          //cb(parse,false);
 	          if(parse == null){
 	          	//error callback
