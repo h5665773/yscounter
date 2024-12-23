46e8ee22fda8a19b1c5816c6f20f5b37086d394d
Lin-Yu-zhe <a29088758a@hotmail.com>
2024-12-23 17:54:04 +0800

    緊急修改 跳過值勤交接

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apiHost: "http://api.wyattst.com.tw/",
-	ajaxOrder: "http://admin.wyattst.com.tw//YSWEB/XMLFORM/YsCounterApi.aspx",
+	ajaxOrder: "http://localhost:50746/XMLFORM/YsCounterApi.aspx",
 	updateURL: "https://github.com/h5665773/yscounter/archive/master.zip",
-	CMSOrder: "http://admin.wyattst.com.tw/CMS/Api/AjaxOrder",
-	testURL: "http://localhost:53278/XMLFORM/YsCounterApi.aspx"
+	CMSOrder: "https://admin.wyattst.com.tw/CMS/Api/AjaxOrder",
+	testURL: "http://localhost:50746/XMLFORM/YsCounterApi.aspx"
 };
