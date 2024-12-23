15f57016661fae94ad5c28eb812464196bc94437
Lin-Yu-zhe <a29088758a@hotmail.com>
2024-12-23 18:11:08 +0800

    fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	apiHost: "http://api.wyattst.com.tw/",
-	ajaxOrder: "http://localhost:50746/XMLFORM/YsCounterApi.aspx",
+	ajaxOrder: "http://admin.wyattst.com.tw//YSWEB/XMLFORM/YsCounterApi.aspx",
 	updateURL: "https://github.com/h5665773/yscounter/archive/master.zip",
 	CMSOrder: "https://admin.wyattst.com.tw/CMS/Api/AjaxOrder",
 	testURL: "http://localhost:50746/XMLFORM/YsCounterApi.aspx"
